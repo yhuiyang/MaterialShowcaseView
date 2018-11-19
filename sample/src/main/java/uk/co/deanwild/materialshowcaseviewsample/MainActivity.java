540879a114c9c0cd19e5f0243cfcf65c7a01ca30
YH Yang <yhuiyang@gmail.com>
2018-11-19 16:19:08 +0800

    Migrate to AndroidX.
    This is sample code in 3rd party library, not useful in our app code base.

--- sample/src/main/java/uk/co/deanwild/materialshowcaseviewsample/MainActivity.java
+++ sample/src/main/java/uk/co/deanwild/materialshowcaseviewsample/MainActivity.java
@@ -3,7 +3,7 @@ package uk.co.deanwild.materialshowcaseviewsample;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
